9265261116f4358bc49e7aeef28b2c7d72cba11f
Segger <saintdane@gmail.com>
2015-10-05 19:02:25 -0400

    Initial phaser implementation
    Just a little falling guy atm, use arrow keys to move it.

--- grunp.js
+++ grunp.js
@@ -3,13 +3,55 @@
 				Gun Run Jump
 */
 
-grunp = new optiWink();
+grunpGame = function(){
 
-boxy = new grunp.entity(20,50,true);
-boxy.update = function(){
+	this.preload = function(){
+		grunp.load.image("boxy","gfx/boxy.png")
 
+
+	};
+
+	this.create = function(){
+		grunp.physics.startSystem(Phaser.Physics.ARCADE);
+		grunp.physics.arcade.gravity.y = 250;
+
+
+		//Create main character 'boxy'
+		boxy = grunp.add.sprite(200,200,"boxy");
+		grunp.physics.enable(boxy,Phaser.Physics.ARCADE);
+		boxy.body.collideWorldBounds = true;
+		boxy.body.bounce.y = 0.2;
+
+
+		//Create obstacle to jump over
+		box = grunp.add.sprite(100,200,"boxy");
+		grunp.physics.enable(box,Phaser.Physics.ARCADE);
+		box.body.collideWorldBounds = true;
+		box.body.bounce.y = 0.2;
+
+		this.controls = grunp.input.keyboard.createCursorKeys();
+	};
+
+	this.update = function(){
+		if (this.controls.left.isDown){
+			boxy.body.velocity.x = -100;
+		}
+		else if(this.controls.right.isDown){
+			boxy.body.velocity.x = 100;
+		} 
+		else if(!(this.controls.right.isDown||this.controls.left.isDown)) {
+			boxy.body.velocity.x = 0;
+		}
+
+
+	};
+
+	this.render = function(){
+
+
+	};
 }
 
-for (var i = 0; i < 10; i++) {
-	new grunp.entity(Math.random()*grunp.canvas.width,Math.random()*grunp.canvas.height);
-};
+var grunp = new Phaser.Game(window.innerWidth-18,window.innerHeight-17, Phaser.AUTO, 'GRunP');
+grunp.state.add("grunpGame",grunpGame);
+grunp.state.start("grunpGame");
